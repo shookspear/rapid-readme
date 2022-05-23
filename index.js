58918d2f3317b39e246b3e85a1991d228f74c117
Echeta Okeke <echeta@rogers.com>
2022-05-23 12:52:50 -0400

    added basic logic to functions

--- index.js
+++ index.js
@@ -1,138 +1,91 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 
-// TODO: Create an array of questions for user input
-//const questions = [];
-
-const questions = () => {
-    return inquirer.prompt([
-      {
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'GitHub Username'
+      },  
+    {
         type: 'input',
         name: 'title',
-        message: 'Project Title',
-        validate: titleInput => {
-          if (titleInput) {
-            return true;
-          } else {
-            console.log('Please enter your Project Title!');
-            return false;
-          }
-        }
+        message: 'Project Title'
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Description',
-        validate: descriptionInput => {
-          if (descriptionInput) {
-            return true;
-          } else {
-            console.log('Please enter your Description!');
-            return false;
-          }
-        }
+        message: 'Description'
       },
       {
         type: 'input',
         name: 'tableOfContents',
-        message: 'Table of Contents',
-        validate: tableOfContentsInput => {
-            if (tableOfContentsInput) {
-              return true;
-            } else {
-              console.log('Please enter your Table of Contents');
-              return false;
-            }
-          }
+        message: 'Table of Contents'
       },
       {
         type: 'input',
         name: 'installation',
-        message: 'Installation',
-        validate: installationInput => {
-            if (installationInput) {
-              return true;
-            } else {
-              console.log('Please enter your Installaton');
-              return false;
-            }
-          }
+        message: 'Installation'
       },
       {
         type: 'input',
         name: 'usage',
-        message: 'Usage',
-        validate: usageInput => {
-            if (usageInput) {
-              return true;
-            } else {
-              console.log('Please enter your Usage');
-              return false;
-            }
-          }
+        message: 'Usage'
       },
       {
-        type: 'input',
+        type: 'list',
         name: 'license',
-        message: 'License',
-        validate: licenseInput => {
-            if (licenseInput) {
-              return true;
-            } else {
-              console.log('Please enter your License');
-              return false;
-            }
-          }
+        message: 'Select the License',
+        choices: ['MIT','Apache','GNU GPL v3']
       },
       {
         type: 'input',
         name: 'contributing',
-        message: 'Contributing',
-        validate: contributingInput => {
-            if (contributingInput) {
-              return true;
-            } else {
-              console.log('Please enter your Contributing');
-              return false;
-            }
-          }
+        message: 'Contributing'
       },
       {
         type: 'input',
         name: 'tests',
-        message: 'Tests',
-        validate: testsInput => {
-            if (testsInput) {
-              return true;
-            } else {
-              console.log('Please enter your Tests');
-              return false;
-            }
-          }
+        message: 'Tests'
       },
       {
         type: 'input',
         name: 'questions',
-        message: 'Questions',
-        validate: questionsInput => {
-            if (questionsInput) {
-              return true;
-            } else {
-              console.log('Please enter your Questions');
-              return false;
-            }
-          }
-      }
-    ]);
-  };
+        message: 'Questions'
+      },
+    ];
+
+// Function to prompt user to answer Questions
+function promptUser() {
+    return inquirer.prompt(questions);
+}
 
 
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if(err){
+            throw err;
+        }
+        console.log("Your README file has been generated")
+    });
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+    promptUser()
+    .then(answers => {
+        return generateMarkdown(answers);
+    })
+    .then(pageMarkdown => {
+        writeToFile('./dist/README.md',pageMarkdown);
+    }) 
+    .catch(err => {
+        console.log(err);
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,66 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  switch(license){
+    case 'MIT':
+      return 'https://img.shields.io/badge/License-MIT-yellow.svg';
+    case 'Apache':
+      return 'https://img.shields.io/badge/License-Apache_2.0-blue.svg';
+    case 'GNU GPL v3':
+      return 'https://img.shields.io/badge/License-GPLv3-blue.svg';
+    default:
+      return '';
+  }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+};
 
-// TODO: Create a function to generate markdown for README
+// Function that returns the license link
+function renderLicenseLink(license) {
+  
+  switch(license){
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'Apache':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'GNU GPL v3':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    default:
+      return '';
+  }
+
+};
+
+// Function that returns the license section of README
+function renderLicenseSection(license) {
+   return `## License
+Licensed under the ${license} license. For more information please visit: ${renderLicenseSection()}   
+    `
+  };
+
+
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  # ${data.description}
+
+  # ${data.tableOfContents}
+
+  # ${data.installation}
+
+  # ${data.usage}
+
+  # ${data.license}
+
+  # ${data.contributing}
+
+  # ${data.tests}
+
+  # ${data.questions}
+
+
 `;
+
 }
 
 module.exports = generateMarkdown;
